67973a5df895e9befc65897e4e0a3aa842623762
leo-guggenberger <info@leo-guggenberger.at>
2017-10-30 13:10:28 +0100

    Create __openerp__.py

---
+++ bee_master/__openerp__.py
@@ -0,0 +1,18 @@
+# __openerp__.py
+{
+    'name': "Bee master",
+    'version': '8.0',
+    'description': """
+        Bee master with following features:
+        - Bee master data
+        """,
+    'author': 'Leo',
+    'website': 'http://www.leo-guggenberger.at',
+    'category': 'Generic Modules/Others',
+    'depends': ['web'],
+    'data': ['static/src/xml/bee_master_view.xml'],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'auto_install': False,
+}
